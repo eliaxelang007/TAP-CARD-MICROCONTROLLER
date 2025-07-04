0c6c9707c57a0dc587fefe14d33c6165354b093b
Eli <eli.axel.ang@gmail.com>
2025-07-04 17:50:19 +0800

    Restored (as best as possible) an older, working version of the microcontroller code.

--- lib/networking/networking.h
+++ lib/networking/networking.h
@@ -1,196 +1,66 @@
 #ifndef _NETWORKING
 #define _NETWORKING
 
-// https://esp32.com/viewtopic.php?t=28570
-// https://github.com/espressif/esp-idf/blob/master/examples/protocols/http_request/main/http_request_example_main.c
-
-#include <Arduino.h>
-
 #include <WiFi.h>
-#include <esp_http_client.h>
-#include <esp_transport_ssl.h>
+#include <HTTPClient.h>
 
-#include <memory>
-
-#include "server_certificate.h"
-#include "slice.h"
 #include "string_help.h"
-#include "serial_io.h"
+#include "section.h"
+#include "uid.h"
 
-#include "result.h"
-#include "method.h"
-
-struct HttpResponse
+struct ServerResponse
 {
-    Option<std::vector<uint8_t>> data;
+    int status;
+    String response;
+
+    explicit ServerResponse(int status, String response) : status{status}, response{response} {}
 };
 
-class HttpClient
+class ServerApi
 {
-    class HttpWorker
-    {
-    private:
-        using ByteBuffer = std::vector<uint8_t>;
-
-        static esp_err_t _event_handler(esp_http_client_event_t *evt) noexcept
-        {
-            static bool new_request = true;
-
-            ByteBuffer &output_buffer = *static_cast<ByteBuffer *>(evt->user_data);
-
-            switch (evt->event_id)
-            {
-            case HTTP_EVENT_ON_DATA:
-            {
-                if (esp_http_client_is_chunked_response(evt->client))
-                {
-                    break;
-                }
-
-                if (new_request)
-                {
-                    output_buffer.clear();
-                    new_request = false;
-                }
-
-                const Slice<uint8_t> data = Slice<uint8_t>(*static_cast<uint8_t *>(evt->data), evt->data_len);
-
-                output_buffer.insert(output_buffer.end(), data.begin(), data.end());
-
-                break;
-            }
-
-            case HTTP_EVENT_ON_FINISH:
-            {
-                new_request = true;
-                break;
-            }
-
-            default:
-            {
-                break;
-            }
-            }
-
-            return ESP_OK;
-        }
-
-        static constexpr esp_http_client_config_t _DEFAULT_CONFIG = {.url = "",
-                                                                     .cert_pem = "",
-                                                                     .cert_len = sizeof(""),
-                                                                     .timeout_ms = 5000,
-                                                                     .event_handler = HttpWorker::_event_handler,
-                                                                     .user_data = nullptr,
-                                                                     .is_async = true};
-
-    public:
-        HttpWorker() : _client{std::shared_ptr<esp_http_client>(esp_http_client_init(&_DEFAULT_CONFIG), esp_http_client_cleanup)}
-        {
-        }
-
-    private:
-        std::shared_ptr<esp_http_client> _client;
-    };
-
-    using SharedWorker = std::shared_ptr<HttpWorker>;
+private:
+    const char *_server_address;
 
 public:
-    explicit HttpClient() : _worker{std::make_shared<HttpWorker>()} {}
-
-    class Request
+    explicit ServerApi(const char *server_address) : _server_address{server_address}
     {
-    private:
-        friend class HttpClient;
-
-        explicit Request(
-            SharedWorker worker,
-            HttpMethod method,
-            str url,
-            str certificate,
-            uint16_t timeout) : _worker{worker}, _method{method}, _url{url}, _certificate{certificate}, _timeout{timeout} {}
-
-        SharedWorker _worker;
-        HttpMethod _method;
-        str _url;
-        str _certificate;
-        uint16_t _timeout;
-    };
-
-    Request request(HttpMethod method, str url, str certificate, uint16_t timeout = 5000)
-    {
-        return Request{
-            _worker,
-            method,
-            url,
-            certificate,
-            timeout,
-        };
     }
 
-private:
-    SharedWorker _worker;
+    void initialize(String ssid, String password)
+    {
+        WiFi.begin(ssid, password);
+
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            delay(500);
+            debug(".");
+        }
+
+        debugln("Connected to the internet!");
+    }
+
+    ServerResponse send_tap(Uid uid, uint16_t timeout)
+    {
+        HTTPClient client;
+
+        client.setTimeout(timeout);
+
+        String address = build_string(
+            _server_address,
+            "/log/",
+            uid.to_chars());
+
+        client.begin(_server_address);
+
+        int status = client.GET();
+        String response = client.getString();
+
+        client.end();
+
+        return ServerResponse{
+            status,
+            response};
+    }
 };
-
-void https_async()
-{
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    // const char *post_data = "Using a Palantír requires a person with great strength of will and wisdom. The Palantíri were meant to "
-    //                         "be used by the Dúnedain to communicate throughout the Realms in Exile. During the War of the Ring, "
-    //                         "the Palantíri were used by many individuals. Sauron used the Ithil-stone to take advantage of the users "
-    //                         "of the other two stones, the Orthanc-stone and Anor-stone, but was also susceptible to deception himself.";
-    esp_http_client_set_method(client, HTTP_METHOD_GET);
-    // esp_http_client_set_post_field(client, post_data, strlen(post_data));
-
-    esp_err_t err = ESP_ERR_HTTP_EAGAIN;
-    while (err == ESP_ERR_HTTP_EAGAIN)
-    {
-        err = esp_http_client_perform(client);
-    }
-
-    if (err == ESP_OK)
-    {
-        // ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %" PRId64,
-        //          esp_http_client_get_status_code(client),
-        //          esp_http_client_get_content_length(client));
-    }
-    else
-    {
-        debugln(esp_err_to_name(err));
-        // ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-    }
-
-    esp_http_client_cleanup(client);
-
-    for (const uint8_t byte : output_buffer)
-    {
-        print(static_cast<char>(byte));
-    }
-
-    print("\n");
-    println("Done!");
-}
-
-// void setup()
-// {
-//     debugln("Hello, world!");
-
-//     WiFi.begin("Ang Network", "KZaccxEf");
-
-//     while (WiFi.status() != WL_CONNECTED)
-//     {
-//         debug(".");
-//         delay(500);
-//     }
-
-//     debugln("\nConnected!");
-//     debugln("Starting http request...");
-//     debugln("1-2-3-4-5");
-
-//     https_async();
-// }
-
-// void loop()
-// {
-// }
 
 #endif
--- lib/utils/result.h
+++ lib/utils/result.h
@@ -37,10 +37,24 @@ public:
             Value{std::in_place_index<1>, error}};
     }
 
+    // template <typename M>
+    // M match(
+    //     std::function<M(T)> on_ok,
+    //     std::function<M(E)> on_error) &&
+    // {
+    //     switch (_tag)
+    //     {
+    //     case Tag::Ok:
+    //         return on_ok(std::get<0>(std::move(_value)));
+    //     case Tag::Error:
+    //         return on_error(std::get<1>(std::move(_value)));
+    //     }
+    // }
+
     template <typename M>
-    M match(
-        std::function<M(T)> on_ok,
-        std::function<M(E)> on_error) &&
+    constexpr M match(
+        M (*on_ok)(T),
+        M (*on_error)(E)) &&
     {
         switch (_tag)
         {
@@ -51,38 +65,33 @@ public:
         }
     }
 
-    template <typename M>
-    constexpr M match(
-        M (*on_ok)(const T &),
-        M (*on_error)(const E &)) const
-    {
-        switch (_tag)
-        {
-        case Tag::Ok:
-            return on_ok(std::get<0>(_value));
-        case Tag::Error:
-            return on_error(std::get<1>(_value));
-        }
-    }
+    // template <typename M>
+    // Result<M, E> map(
+    //     std::function<M(T)> map) &&
+    // {
+    //     using R = Result<M, E>;
 
-    template <typename M>
-    Result<M, E> map(
-        std::function<M(T)> map) &&
-    {
-        using R = Result<M, E>;
+    //     switch (_tag)
+    //     {
+    //     case Tag::Ok:
+    //         return R::ok(map(std::get<0>(std::move(_value))));
+    //     case Tag::Error:
+    //         return R::error(std::get<1>(std::move(_value)));
+    //     }
+    // }
 
-        switch (_tag)
-        {
-        case Tag::Ok:
-            return R::ok(map(std::get<0>(std::move(_value))));
-        case Tag::Error:
-            return R::error(std::get<1>(std::move(_value)));
-        }
+    constexpr T expect(const char *message)
+    {
+        return match(
+            [](T ok)
+            { return ok; },
+            [](E error)
+            { return panic(message); });
     }
 
     template <typename M>
     constexpr Result<M, E> map(
-        M (*map)(const T &)) const
+        M (*map)(T)) const
     {
         using R = Result<M, E>;
 
@@ -114,6 +123,13 @@ template <typename T>
     while (true)
     {
     }
+}
+
+template <typename P, typename T>
+[[noreturn]] constexpr T panic(P printable) noexcept
+{
+    println(printable);
+    return never();
 }
 
 template <typename T>
